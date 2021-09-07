f6c60829df9954e879b9bccd858eef68ee2b02fb
Vlad Korotkov <fruityw1337@yahoo.com>
2021-09-07 16:02:33 +0300

    configure for heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const fs = require('fs');
 
 const app = express()
-const PORT = 5010
+const PORT = process.env.PORT || 5000
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
